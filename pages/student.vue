846e64a6c77a350b3b1e7e20c2e4e093163147de
Cieran Sung <clsung154@gmail.com>
2025-02-13 18:34:41 +0000

    add select and unselect a group in sidebar

--- pages/student.vue
+++ pages/student.vue
@@ -3,7 +3,17 @@
     <!-- Sidebar -->
     <div class="bg-black w-[516px] p-12 flex flex-col">
       <!-- SidebarTitle -->
-      <h1 class="text-white font-bold text-lg mb-5">My Groups</h1>
+      <div class="flex items-center justify-between mb-5">
+        <h1 class="text-white font-bold text-lg">My Groups</h1>
+        <button class="text-[#DA9914] text-xs font-bold space-x-2 cursor-pointer" :class="
+          {
+            'inline-flex items-center' : typeof selectedGroup.id === 'number',
+            'hidden' : typeof selectedGroup.id !== 'number'
+          }" @click="selectedGroup = {}">
+          <LeftArrow class="w-4 h-4"/>
+          <span>Back</span>
+        </button>
+      </div>
       <!-- /SidebarTitle -->
 
       <!-- SidebarContent -->
@@ -12,12 +22,16 @@
         Please assign yourself to the desired group
       </p>
       <div v-else v-for="group in userGroups">
-        <div class="mt-5 p-2 rounded-lg cursor-pointer hover:bg-[#A1842C]/50">
+        <div class="mt-5 p-2 rounded-lg cursor-pointer" :class="
+          { 
+            'bg-[#A1842C]': group.myGroup.id === selectedGroup.id,
+            'hover:bg-[#A1842C]/50' : group.myGroup.id !== selectedGroup.id,
+          }" @click="setGroup(group)">
           <h3 class="text-[#F4F4F5] text-sm font-bold truncate">{{ group.moduleName }}</h3>
           <div class="leading-none truncate text-[#C2C2C5]">
-            <span class="text-[#F4F4F5] text-xs mr-4">Group {{ group.groups.id }}</span>
-            <span class="text-[#C2C2C5] text-xs mr-4">{{ `${group.groups.userGroup.length}/${group.moduleGroupSize}` }} Members</span>
-            <span class="text-[#C2C2C5] text-xs">Joined {{ new Date(group.groups.userGroup.find(g => g.userId === userId).updatedAt).toLocaleString('en-GB') }}</span>
+            <span class="text-[#F4F4F5] text-xs mr-4">Group {{ group.myGroup.id }}</span>
+            <span class="text-[#C2C2C5] text-xs mr-4">{{ `${group.myGroup.userGroup.length}/${group.moduleGroupSize}` }} Members</span>
+            <span class="text-[#C2C2C5] text-xs">Joined {{ new Date(group.myGroup.userGroup.find(g => g.userId === userId).updatedAt).toLocaleString('en-GB') }}</span>
           </div>
         </div>
       </div>
@@ -42,9 +56,13 @@
 </template>
 
 <script setup>
+  const selectedGroup = ref({});
   const modulesData = ref([]);
   const userId = ref(Number);
 
+  function setGroup(group) {
+    selectedGroup.value = group.myGroup;
+  }
 
   definePageMeta({
     middleware: 'auth',
@@ -56,7 +74,7 @@
         {
           moduleName: module.name,
           moduleGroupSize: module.groupSize,
-          groups: module.groups.find(group => group.isPartOf) || null
+          myGroup: module.groups.find(group => group.isPartOf) || null
         }
       ))
       .filter(module => module.groups !== null);
@@ -69,7 +87,7 @@
       modulesData.value = response.data;
       userId.value = response.user;
 
-
+      console.log(selectedGroup);
     } catch (error) {
       console.log(error);
     }
